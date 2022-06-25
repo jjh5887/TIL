98e970f6c1fa102e2ebc8a8adf6e5c00c943bae7
jjh5887 <wjdrnjsgh159@naver.com>
2022-06-25 17:00:10 +0900

    디자인 패턴: 자바 and 스프링 이터레이터 패턴

---
+++ design-patterns/src/main/java/me/powerarc/designpatterns/behavioral_patterns/iterator/_03_java/IteratorInSpring.java
@@ -0,0 +1,10 @@
+package me.powerarc.designpatterns.behavioral_patterns.iterator._03_java;
+
+import org.springframework.util.CompositeIterator;
+
+public class IteratorInSpring {
+
+	public static void main(String[] args) {
+		CompositeIterator iterator;
+	}
+}
