e7425581eba74e02da8379a9967effd2dca96be7
jjh5887 <wjdrnjsgh159@naver.com>
2022-11-26 17:05:15 +0900

    TS 함수 옵셔널 파라미터

--- typescript/class-note/02_functions.ts
+++ typescript/class-note/02_functions.ts
@@ -4,3 +4,15 @@ function sum(a: number, b: number): number {
 
 let number = sum(10, 20);
 
+// 파리미터 제한
+sum(10, 20, 30, 40);
+sum(10);
+
+// 함수의 옵셔널 파라미터 -> ? -> 써도되고 안써도 되고
+function log(a: string, b?: string, c?: string) {
+
+}
+
+log('hi');
+log('hi', 'bye');
+log('hi', 'bye', 'hello');
