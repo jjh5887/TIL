ff6efb9fb1f75967ab585943ad0a461f99ec7f49
jjh5887 <wjdrnjsgh159@naver.com>
2022-11-26 16:45:34 +0900

    TS 기본 타입

---
+++ typescript/class-note/01_type-basic.ts
@@ -0,0 +1,24 @@
+let str: string = 'hello';
+let num: number = 10;
+let show: boolean = true;
+
+// 배열
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<String> = ['Capt', 'Thor', 'Hulk'];
+
+let items: number[] = [1, 2, 3];
+
+// 튜플
+let address: [string, number] = ['gangnam', 100];
+
+// 객체
+let obj: object = {};
+// let person: object = {
+//     name: 'capt',
+//     age: 100
+// }
+
+let person: { name: string, age: number } = {
+    name: 'thor',
+    age: 1000
+}
