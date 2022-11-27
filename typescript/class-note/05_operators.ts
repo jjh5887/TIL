96b1792daddd3fd6ea6ea51cdd376085cfd4a316
jjh5887 <wjdrnjsgh159@naver.com>
2022-11-27 16:44:39 +0900

    TS 인터섹션 타입

--- typescript/class-note/05_operators.ts
+++ typescript/class-note/05_operators.ts
@@ -38,3 +38,13 @@ function askSomeone(someone: Developer | Person) {
     // someone.skill
     // someone.age
 }
+
+// 인터섹션 타입
+let seho2: string | number | boolean;
+let capt: string & number & boolean; // never
+
+function askSomeone2(someone: Developer & Person) {
+    someone.name;
+    someone.skill;
+    someone.age
+}
