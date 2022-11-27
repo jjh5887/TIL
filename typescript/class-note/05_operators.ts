cbf3c79c51fcadd846440e0e45a707df8ed5051d
jjh5887 <wjdrnjsgh159@naver.com>
2022-11-27 16:38:46 +0900

    TS 유니온 타입

---
+++ typescript/class-note/05_operators.ts
@@ -0,0 +1,40 @@
+function logMessage(vale: any) {
+    console.log(vale);
+}
+
+logMessage('hello');
+logMessage(100);
+
+// 유니온 타입
+let seho: string | number | boolean;
+
+function logMessage2(value: string | number) {
+    if (typeof value === 'number') {
+        value.toLocaleString();
+    }
+    if (typeof value === 'string') {
+        value.toString();
+    }
+
+    throw new TypeError('value must be string or number');
+}
+
+logMessage2('hello');
+logMessage2(100);
+
+interface Developer {
+    name: string;
+    skill: string;
+}
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+function askSomeone(someone: Developer | Person) {
+    someone.name;
+    // 보장된(공통) 속성만 제공
+    // someone.skill
+    // someone.age
+}
