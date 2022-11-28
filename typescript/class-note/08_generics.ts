9161a4e7593a76c757974616b6195a084aa51fd3
jjh5887 <wjdrnjsgh159@naver.com>
2022-11-28 16:29:14 +0900

    TS Generic - 타입제한

--- typescript/class-note/08_generics.ts
+++ typescript/class-note/08_generics.ts
@@ -14,3 +14,26 @@ function logText2<T>(text: T): T {
 
 logText2<string>('10');
 
+function logText3<T>(text: T[]): T[] {
+    console.log(text.length);
+    text.forEach(function (text) {
+        console.log(text);
+    })
+    return text;
+}
+logText3<string>(['hi', 'abc']);
+
+interface LengthType {
+    length: number;
+}
+
+function logTextLength<T extends LengthType>(text: T): T {
+    text.length;
+    return text;
+}
+
+// .length 가 있음
+logTextLength('a');
+// .length 가 없음
+// logTextLength(10);
+logTextLength({length: 10});
