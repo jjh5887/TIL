7b00b4a30d6b3fa4cf563375cbf4c6c32720f027
jjh5887 <wjdrnjsgh159@naver.com>
2022-12-19 16:04:32 +0900

    NodeBird: Link와 Eslint

--- react/nodebird/front/components/AppLayout.js
+++ react/nodebird/front/components/AppLayout.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Link from "next/link";
 
 const AppLayout = ({ children }) => {
     return (
         <div>
-            <div>공통메뉴</div>
-            {children}
+            <div>
+                <Link href="/"><a>노드버드</a></Link>
+                <Link href="/profile"><a>프로필</a></Link>
+                <Link href="/signup"><a>회원가입</a></Link>
+
+            </div>
+            <div>
+                {children}
+            </div>
         </div>
     )
 }
--- react/nodebird/front/pages/profile.js
+++ react/nodebird/front/pages/profile.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import AppLayout from "../components/AppLayout";
 
 const Profile = () => {
-    return <div>프로필 페이지</div>
+    return (
+        <AppLayout>
+            <div>프로필 페이지</div>
+        </AppLayout>
+    );
 };
 
 export default Profile;
