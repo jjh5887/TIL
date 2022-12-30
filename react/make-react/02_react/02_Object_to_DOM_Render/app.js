25d23ba0b27dc1522b21cc939ea36ec2bbbd72ba
jjh5887 <wjdrnjsgh159@naver.com>
2022-12-31 02:45:02 +0900

    Make-React: 객체 to DOM Render

---
+++ react/make-react/02_react/02_Object_to_DOM_Render/app.js
@@ -0,0 +1,50 @@
+function createDOM(node) {
+    if (typeof node === 'string') {
+        return document.createTextNode(node);
+    }
+    const element = document.createElement(node.tag);
+
+    node.children
+        .map(createDOM)
+        // bind를 해주지 않으면 Context 깨짐
+        .forEach(element.appendChild.bind(element));
+
+    return element;
+}
+
+const vdom = {
+    tag: 'p',
+    props: {},
+    children: [
+        {
+            tag: 'h1',
+            props: {},
+            children: ["React 만들기"],
+        },
+        {
+            tag: 'ul',
+            props: {},
+            children: [
+                {
+                    tag: 'li',
+                    props: {},
+                    children: ['첫번째 아이템']
+                },
+                {
+                    tag: 'li',
+                    props: {},
+                    children: ['두번째 아이템']
+                },
+                {
+                    tag: 'li',
+                    props: {},
+                    children: ['세번째 아이템']
+                }
+            ],
+        }
+    ],
+}
+
+document
+    .querySelector('#root')
+    .appendChild(createDOM(vdom));
