b5b2abc0a73dede37a62f8959d286a0ae551c47e
jjh5887 <wjdrnjsgh159@naver.com>
2022-12-31 13:47:05 +0900

    Make-React: babel & webpack 세팅

---
+++ react/make-react/02_react/03_webpack_babel/webpack.config.js
@@ -0,0 +1,38 @@
+// nodeJS 가 제공하는 안전한 경로
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './app.js', // 입력 (어떤 js에서 시작할거야)
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        compress: true,
+        port: 9999,
+    },
+    module: {
+        // loader 들
+        rules: [
+            {
+                test: /\.js$/, // 해당 정규식 파일만 loader 에게
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '2.3 setup webpack & babel',
+            template: 'index.html',
+        })
+    ]
+}
