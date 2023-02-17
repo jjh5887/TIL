5699731691ac1e094ed18ac8f2e63968515e18ec
jjh5887 <wjdrnjsgh159@naver.com>
2023-02-17 22:01:13 +0900

    Make-React: 함수 컴포넌트 vs 클래스 컴포넌트

--- react/make-react/02_react/03_webpack_babel/src/react.js
+++ react/make-react/02_react/03_webpack_babel/src/react.js
@@ -38,6 +38,10 @@ export function createElement(tag, props, ...children) {
     if (typeof tag === 'function') {
         // 실제 리액트는 Symbol을 이용해서 클래스 컴포넌트를 체크함
         if (tag.prototype instanceof Component) {
+            // 클래스 컴포넌트 vs 함수 컴포넌트
+            // 클래스 컴포넌트는 렌더링이 될 때마다 new를 해서 인스턴스를 새로 만들지 않고 render()만 호출하여 상태를 유지 (이 코드에선 안함)
+            // 또 라이프 사이클을 이용하여 원하는 시점에 작업을 할 수 있음
+            // 함수는 이런 상태를 가질 수 없지만 요즘은 Hook을 이용하여 다 가능해짐
             const instance = new tag(makeProps(props, children));
             return instance.render();
         } else {
