7bb58b1435ad1c2b3f432e4b024bf2b646b8c90f
jjh5887 <wjdrnjsgh159@naver.com>
2023-02-19 16:33:20 +0900

    Make-Redux-Middleware: 로거 미들웨어

--- react/make-react/04_redux-middleware/4.1/app.js
+++ react/make-react/04_redux-middleware/4.1/app.js
@@ -1,34 +1,9 @@
 import { createStore } from './redux.js';
 import * as Actions from './actions.js';
 import reducer from './reducer.js';
-import { ASYNC_INCREASE_COUNTER, SET_COUNTER } from './action-type.js';
+import { logger } from './logger.js';
 
-const middleware1 = store => next => action => {
-    console.log("m1 => ", action);
-    next(action);
-};
-
-const middleware2 = store => next => action => {
-  console.log("m2 => ", action);
-  if (action.type === SET_COUNTER) {
-    action.payload = 100;
-  }
-  next(action);
-};
-
-const middleware3 = store => next => action => {
-  console.log("m3 => ", action);
-  if (action.type === ASYNC_INCREASE_COUNTER) {
-    setTimeout(() => {
-      next(Actions.increase());
-    }, 1000);
-  } else {
-    // else를 하지 않으면 reducer 전체가 동작 안함
-    next(action);
-  }
-};
-
-const store = createStore(reducer, [middleware1, middleware2, middleware3]);
+const store = createStore(reducer, [ logger ]);
 
 const counterDisplay = document.querySelector('#counter');
 const btnIncrease = document.querySelector('#btn-increase');
@@ -36,26 +11,25 @@ const btnAsyncIncrease = document.querySelector('#btn-async-increase');
 const btnDecrease = document.querySelector('#btn-decrease');
 const btnReset = document.querySelector('#btn-reset');
 
-store.subscribe(function() {
-  const { counter } = store.getState();
-
-  counterDisplay.textContent = counter;
+store.subscribe(function () {
+    const { counter } = store.getState();
+    counterDisplay.textContent = counter;
 });
 
 store.dispatch(Actions.setCounter(0));
 
 btnReset.addEventListener('click', () => {
-  store.dispatch(Actions.setCounter(0));
+    store.dispatch(Actions.setCounter(0));
 });
 
 btnIncrease.addEventListener('click', () => {
-  store.dispatch(Actions.increase());
+    store.dispatch(Actions.increase());
 });
 
 btnAsyncIncrease.addEventListener('click', () => {
-  store.dispatch(Actions.asyncIncrease({ url: '/async-increase' }));
+    store.dispatch(Actions.asyncIncrease({ url: '/async-increase' }));
 });
 
 btnDecrease.addEventListener('click', () => {
-  store.dispatch(Actions.decrease());
+    store.dispatch(Actions.decrease());
 });
