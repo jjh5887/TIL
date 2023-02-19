7b92cbf8e55dd018628656067b6b9dce3764a16a
jjh5887 <wjdrnjsgh159@naver.com>
2023-02-19 16:23:01 +0900

    Make-Redux-Middleware: 미들웨어 파이프라인 구현

--- react/make-react/04_redux-middleware/4.1/redux.js
+++ react/make-react/04_redux-middleware/4.1/redux.js
@@ -3,7 +3,7 @@ export const actionCreator = type => payload => ({
   payload,
 });
 
-export function createStore(reducer) {
+export function createStore(reducer, middlewares = []) {
   let state;
   const handlers = [];
 
@@ -25,6 +25,15 @@ export function createStore(reducer) {
     subscribe,
     dispatch,
   };
+
+  // Monkey patching
+  let lastDispatch = dispatch;
+  middlewares = Array.from(middlewares).reverse();
+  middlewares.forEach(middleware => {
+    lastDispatch = middleware(store)(lastDispatch);
+  });
+
+  store.dispatch = lastDispatch;
 
   return store;
 }
