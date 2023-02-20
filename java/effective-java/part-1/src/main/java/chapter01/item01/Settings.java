892d841171bac67918130e2f1d0b20b0d937cef8
jjh5887 <wjdrnjsgh159@naver.com>
2023-02-20 23:40:10 +0900

    Effective Java: 정적 팩토리 메서드 단점

--- java/effective-java/part-1/src/main/java/chapter01/item01/Settings.java
+++ java/effective-java/part-1/src/main/java/chapter01/item01/Settings.java
@@ -1,5 +1,9 @@
 package chapter01.item01;
 
+/*
+*  이 클래스의 인스턴스는 #getInstance()를 통해 사용한다.
+*  @see #getINstance()
+* */
 public class Settings {
 
 	private boolean useAutoSteering;
@@ -9,10 +13,12 @@ public class Settings {
 	private Difficulty difficulty;
 
 	// 객체 생성을 제한 가능
-	private Settings() {};
+	// 단점 1. 상속이 어렵다
+	// 단점 2. javadoc 에서 읽는 방법이 어려움 (네이밍 컨벤션으로 어느정도 극복?, 가장 좋은 방법은 문서화)
+	private Settings() {}
 	private static final Settings SETTINGS = new Settings();
 
-	public static Settings newInstance() {
+	public static Settings getInstance() {
 		return SETTINGS;
 	}
 }
