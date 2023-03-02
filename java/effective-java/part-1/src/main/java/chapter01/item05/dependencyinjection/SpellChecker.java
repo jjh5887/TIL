91078c35d0a40b23a0e9fe855cb7513ee2217075
jjh5887 <wjdrnjsgh159@naver.com>
2023-03-02 22:46:12 +0900

    Effective Java: 의존 객체 주입 팩토리 메소드로 한번더 추상화

--- java/effective-java/part-1/src/main/java/chapter01/item05/dependencyinjection/SpellChecker.java
+++ java/effective-java/part-1/src/main/java/chapter01/item05/dependencyinjection/SpellChecker.java
@@ -1,6 +1,7 @@
 package chapter01.item05.dependencyinjection;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 import chapter01.item05.Dictionary;
 
@@ -12,6 +13,12 @@ public class SpellChecker {
 	// 테스트 코드에서 Mocking 도 편리
 	public SpellChecker(Dictionary dictionary) {
 		this.dictionary = dictionary;
+	}
+
+	// 팩토리 메소드 패턴으로 한번더 추상화
+	// 이렇게 되면 어떻게든 Dictionary 만 제공하는 함수형 인터페이스를 제공하면 됨
+	public SpellChecker(Supplier<Dictionary> dictionarySupplier) {
+		this.dictionary = dictionarySupplier.get();
 	}
 
 	public boolean isValid(String word) {
