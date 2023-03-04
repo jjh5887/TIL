ad01c4e28299d4b7d9da7a9294862eaead6cc436
jjh5887 <wjdrnjsgh159@naver.com>
2023-03-04 16:03:48 +0900

    Effective Java: NPE with Optional

---
+++ java/effective-java/part-1/src/main/java/chapter01/item07/optional/MemberShip.java
@@ -0,0 +1,7 @@
+package chapter01.item07.optional;
+
+public class MemberShip {
+	public String hello() {
+		return "hello";
+	}
+}
---
+++ java/effective-java/part-1/src/test/java/chapter01/item07/optional/ChannelTest.java
@@ -0,0 +1,15 @@
+package chapter01.item07.optional;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+
+class ChannelTest {
+
+	@Test
+	void npe() {
+		Channel channel = new Channel();
+		Optional<MemberShip> optional = channel.defaultMemberShip();
+		optional.ifPresent(MemberShip::hello);
+	}
+}
