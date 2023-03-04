2d4e19602d03467a072701256e42931f0493ff08
jjh5887 <wjdrnjsgh159@naver.com>
2023-03-04 13:45:56 +0900

    Effective Java: 불필요한 객체 생성(auto boxing unboxing)

---
+++ java/effective-java/part-1/src/main/java/chapter01/item06/Sum.java
@@ -0,0 +1,31 @@
+package chapter01.item06;
+
+public class Sum {
+	private static long sumWithAutoBoxing() {
+		Long sum = 0L;
+		for (long i = 0; i <= Integer.MAX_VALUE; i++)
+			sum += i;
+		return sum;
+	}
+
+	private static long sumWithoutAutoBoxing() {
+		long sum = 0L;
+		for (long i = 0; i <= Integer.MAX_VALUE; i++)
+			sum += i;
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		long start = System.nanoTime();
+		long x = sumWithAutoBoxing();
+		long end = System.nanoTime();
+		System.out.println((end - start) / 1_000_000. + " ms.");
+		System.out.println(x);
+
+		start = System.nanoTime();
+		x = sumWithoutAutoBoxing();
+		end = System.nanoTime();
+		System.out.println((end - start) / 1_000_000. + " ms.");
+		System.out.println(x);
+	}
+}
