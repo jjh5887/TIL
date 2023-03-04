eff70632b3d1b62f5b78b379a5dfbe1ebe7dc47c
jjh5887 <wjdrnjsgh159@naver.com>
2023-03-04 14:00:28 +0900

    Effective Java: Deprecation

---
+++ java/effective-java/part-1/src/main/java/chapter01/item06/Client.java
@@ -0,0 +1,9 @@
+package chapter01.item06;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Deprecation deprecation = new Deprecation("string");
+		Deprecation deprecation2 = new Deprecation();
+	}
+}
