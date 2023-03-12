aba9caedc3b3279ee631a68c7feb3b9f15f61fc4
jjh5887 <wjdrnjsgh159@naver.com>
2023-03-12 14:43:05 +0900

    NodeBird: redux 설치

--- react/nodebird/front/pages/_app.js
+++ react/nodebird/front/pages/_app.js
@@ -1,9 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Head from "next/head";
+import wrapper from "../store/configureStore";
 
 const App = ({ Component }) => {
     return (
+        // Next를 안쓰면 필요하지만 Next에선 필요없음(알아서 넣어줌)
+        // <Provider store={}></Provider>
         <>
             <Head>
                 <meta charSet="utf-8"/>
@@ -20,4 +23,4 @@ App.prototype = {
     Component: PropTypes.elementType.isRequired,
 }
 
-export default App;
+export default wrapper.withRedux(App);
