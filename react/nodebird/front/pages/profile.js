65b505e23dc2466dc67dfb5a8ec3ebc4beface16
jjh5887 <wjdrnjsgh159@naver.com>
2023-03-12 12:57:15 +0900

    NodeBird: 프로필 페이지 만들기

--- react/nodebird/front/pages/profile.js
+++ react/nodebird/front/pages/profile.js
@@ -1,11 +1,21 @@
-import React from 'react';
 import AppLayout from "../components/AppLayout";
+import FollowList from "../components/FollowList";
+import NicknameEditForm from "../components/NicknameEditForm";
 
 const Profile = () => {
+    const followerList = [{ nickname: '권호' }, { nickname: '권호1' }, { nickname: '권호2' }];
+    const followingList = [{ nickname: '권호3' }, { nickname: '권호4' }, { nickname: '권호5' }];
     return (
-        <AppLayout>
-            <div>프로필 페이지</div>
-        </AppLayout>
+        <>
+            <header>
+                <title>내 프로필 | NodeBird</title>
+            </header>
+            <AppLayout>
+                <NicknameEditForm/>
+                <FollowList header={ "팔로잉 목록" } data={ followingList } />
+                <FollowList header={ "팔로워 목록" } data={ followerList } />
+            </AppLayout>
+        </>
     );
 };
 
