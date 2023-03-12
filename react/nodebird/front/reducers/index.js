90cb52e3086a8db2b2b49014da6afeac5f3b101d
jjh5887 <wjdrnjsgh159@naver.com>
2023-03-12 16:19:01 +0900

    NodeBird: 리듀서 쪼개기

--- react/nodebird/front/reducers/index.js
+++ react/nodebird/front/reducers/index.js
@@ -1,57 +1,23 @@
 import { HYDRATE } from "next-redux-wrapper";
-
-const initState = {
-    user: {
-        isLoggedIn: false,
-        user: null,
-        signUpData: {},
-        loginData: {},
-    },
-    post: {
-        mainPosts: [],
-    },
-};
-
-export const loginAction = (data) => {
-    return {
-        type: 'LOG_IN',
-        data,
-    }
-}
-export const logoutAction = () => {
-    return {
-        type: 'LOG_OUT',
-    }
-}
+import user from '../reducers/user';
+import post from '../reducers/post';
+import { combineReducers } from "redux";
 
 // (이전상태, 액션) => 다음상태
-const rootReducer = (state = initState, action) => {
-    switch (action.type) {
-        // 지금은 그냥 넣기만
-        case HYDRATE:
-            console.log('HYDRATE', action);
-            return { ...state, ...action.payload };
-        case 'LOG_IN':
-            return {
-                ...state,
-                user: {
-                    ...state.user,
-                    isLoggedIn: true,
-                    user: action.data,
-                },
-            };
-        case 'LOG_OUT':
-            return {
-                ...state,
-                user: {
-                    ...state.user,
-                    isLoggedIn: false,
-                    user: null,
-                },
-            }
-        default:
-            return state;
-    }
-}
+const rootReducer = combineReducers({
+    // index 부분은 원래 없어도 되지만 서버 사이드 렌더링(HYDRATE)를 위해서 사용
+    index: (state = {}, action) => {
+        switch (action.type) {
+            // 지금은 그냥 넣기만
+            case HYDRATE:
+                console.log('HYDRATE', action);
+                return { ...state, ...action.payload };
+            default:
+                return state;
+        }
+    },
+    user,
+    post,
+});
 
 export default rootReducer;
---
+++ react/nodebird/front/reducers/post.js
@@ -0,0 +1,12 @@
+export const initState = {
+    mainPosts: [],
+};
+
+const reducer = (state = initState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default reducer;
