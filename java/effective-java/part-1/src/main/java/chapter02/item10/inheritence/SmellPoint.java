5b5d5547b04b75764b42cf83e84416d0f0e3689b
정권호 <wjdrnjsgh159@naver.com>
2023-03-25 14:31:46 +0900

    Effective Java: equals StackOverflow 문제

--- java/effective-java/part-1/src/main/java/chapter02/item10/inheritence/SmellPoint.java
+++ java/effective-java/part-1/src/main/java/chapter02/item10/inheritence/SmellPoint.java
@@ -18,7 +18,7 @@ public class SmellPoint extends Point {
 
 		// o가 일반 Point면 색상을 무시하고 비교한다.
 		if (!(o instanceof SmellPoint))
-			// 또다른 자식클래스의 equals도 내부에서 또 equals를 호출하면 StackOverFlow
+			// 또다른 클래스의 equals도 내부에서 또 equals를 호출하면 StackOverFlow
 			return o.equals(this);
 
 		// o가 ColorPoint면 색상까지 비교한다.
